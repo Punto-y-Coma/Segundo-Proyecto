66b83f7a6b4948cf89d3468eb18697d2a404c44d
V <V@MacBook-Pro-de-V.local>
2021-11-03 11:53:35 +0100

    added newsletter content

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-function store(){
+function email(){
     let inputEmail= document.getElementById("email");
     localStorage.setItem("email", inputEmail.value);
+   }
+
+   function Submit(){
+    let inputEmail= document.getElementById("email");
+    localStorage.setItem("email", inputEmail.value);
+   }
+
+   function persistInput(email)
+   {
+     let key = "input-" + input.id;
+   
+     let storedValue = localStorage.getItem(key);
+
+     let inputElement = document.getElementById("email");
+
+     persistInput(inputElement);
+   
+     if (storedValue)
+         input.value = storedValue;
+   
+     input.addEventListener('input', function ()
+     {
+         localStorage.setItem(key, input.value);
+     });
    }
